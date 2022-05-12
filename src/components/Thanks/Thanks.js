d87311047a0d79154923cea36bb4ced97ba4fbc7
hubi <hubert1234.94@gmail.com>
2022-05-12 02:44:33 +0200

    Add Thanks component

---
+++ src/components/Thanks/Thanks.js
@@ -0,0 +1,14 @@
+import classes from "./Thanks.module.scss";
+
+const Thanks = () => {
+  return (
+    <section className={classes["thanks"]}>
+      <div>
+        <h1>Thank you very much!</h1>
+        <div>I will try to respond as soon as possible.</div>
+      </div>
+    </section>
+  );
+};
+
+export default Thanks;
