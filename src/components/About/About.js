4a5814a10d084045605453f81ac44b80ff8a89f8
hubi <hubert1234.94@gmail.com>
2022-05-12 02:40:23 +0200

    Add About component

---
+++ src/components/About/About.js
@@ -0,0 +1,15 @@
+import AboutHeader from "./AboutHeader";
+import WorkExperiences from "./WorkExperiences";
+import Skills from "./Skills";
+
+const About = () => {
+  return (
+    <>
+      <AboutHeader />
+      <WorkExperiences />
+      <Skills />
+    </>
+  );
+};
+
+export default About;
