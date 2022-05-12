e74554a17a920b58015b575951053dabdc1259f3
hubi <hubert1234.94@gmail.com>
2022-05-12 02:44:46 +0200

    Change Footer component

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -4,28 +4,7 @@ const Footer = () => {
   return (
     <footer className={classes["footer"]}>
       <div className={classes["footer__container"]}>
-        <div className={classes["footer__container--row"]}>
-          <div className={classes["footer__container--col"]}>
-            <ul>
-              <li>Icon</li>
-            </ul>
-          </div>
-          <div className={classes["footer__container--col"]}>
-            <ul>
-              <li>Icon</li>
-            </ul>
-          </div>
-          <div className={classes["footer__container--col"]}>
-            <ul>
-              <li>Icon</li>
-            </ul>
-          </div>
-        </div>
-        <div className={classes["footer__container--info"]}>
-          <div className={classes["footer__container--info__col"]}>
-            &copy;{new Date().getFullYear()} All rights reserved
-          </div>
-        </div>
+            &copy;Made by me
       </div>
     </footer>
   );
